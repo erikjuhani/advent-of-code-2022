df88c70ac086775e5372fd11443855a356904b7f
Erik Kinnunen <erik.kinnunen@reaktor.com>
2022-12-04 18:07:16 +0200

    Solve day 4 puzzles
    Solution:
      DAY 4 (1/2): 538
      DAY 4 (2/2): 792
    
    Add TODAY env variable to have better access to just run _today_
    solution.

---
+++ day4.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+type Range struct {
+	start, end int
+}
+
+func (r Range) Contains(rb Range) bool {
+	if r.start >= rb.start && r.end <= rb.end {
+		return true
+	}
+
+	return false
+}
+
+func (r Range) ContainsAny(rb Range) bool {
+	if r.end >= rb.start && r.start <= rb.end {
+		return true
+	}
+
+	return false
+}
+
+func NewRange(rawRange []string) (Range, error) {
+	start, err := strconv.Atoi(rawRange[0])
+	if err != nil {
+		return Range{}, err
+	}
+
+	end, err := strconv.Atoi(rawRange[1])
+	if err != nil {
+		return Range{}, err
+	}
+
+	return Range{start, end}, nil
+}
+
+func Day4() error {
+	input, err := ReadInput("./input/day04")
+	if err != nil {
+		return err
+	}
+
+	var amount1, amount2 int
+	for _, s := range strings.Split(input, "\n") {
+		pair := strings.Split(s, ",")
+		if len(pair) != 2 {
+			continue
+		}
+		rangeA, err := NewRange(strings.Split(pair[0], "-"))
+		if err != nil {
+			return err
+		}
+		rangeB, err := NewRange(strings.Split(pair[1], "-"))
+		if err != nil {
+			return err
+		}
+
+		if rangeA.Contains(rangeB) || rangeB.Contains(rangeA) {
+			amount1++
+		}
+
+		if rangeA.ContainsAny(rangeB) || rangeB.ContainsAny(rangeA) {
+			amount2++
+		}
+	}
+
+	fmt.Printf("DAY 4 (1/2): %d\n", amount1)
+	fmt.Printf("DAY 4 (2/2): %d\n", amount2)
+
+	return nil
+}
--- main.go
+++ main.go
@@ -1,15 +1,37 @@
 package main
 
-var startFromDay = 1
+import (
+	"os"
+	"time"
+)
 
 var adventCalendar []func() error = []func() error{
 	Day1,
 	Day2,
 	Day3,
+	Day4,
+}
+
+func adventDay() int {
+	currentDay := time.Now().Day()
+	l := len(adventCalendar)
+
+	if currentDay < 26 && currentDay <= l {
+		return currentDay
+	}
+
+	return l
 }
 
 func main() {
-	for _, fn := range adventCalendar[startFromDay-1:] {
+	TODAY := os.Getenv("TODAY")
+	d := 0
+
+	if TODAY == "1" {
+		d = adventDay() - 1
+	}
+
+	for _, fn := range adventCalendar[d:] {
 		if err := fn(); err != nil {
 			panic(err)
 		}
